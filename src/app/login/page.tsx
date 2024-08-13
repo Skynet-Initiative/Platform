136681e7dc1377407bcf1431eae1f7f978f03db6
Linaria64 <dorianpro77@proton.me>
2024-08-13 22:27:05 +0200

    #1 setup left side login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,17 @@ export default function Login() {
     return (
       
         <div className="flex w-full h-screen ">
-          <div className="border-4 border-pink-400 w-2/4"><h1>Partie Gauche</h1></div>
+          <div className="border-4 border-pink-400 w-2/4 flex flex-col mx-auto">
+            <div className="w-full h-1/3">
+              <h1>Logo</h1>
+            </div> 
+            <div className="w-full h-1/3"><h1>Image</h1></div>
+            <div className="w-full h-1/3"><p>
+              Lorem ipsum dolor sit, 
+              amet consectetur adipisicing elit. Quibusdam, beatae soluta rem possimus excepturi deserunt, 
+              molestias non inventore fuga eum repudiandae quia suscipit autem quasi laudantium? Ab iusto expedita ipsam.
+              </p></div>
+          </div>
           <div><h1>Partie droite</h1></div>
         </div>
     );
