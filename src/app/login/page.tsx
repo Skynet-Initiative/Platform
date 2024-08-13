b1331eb9fae199930c6c18c3d0c070510b963f7a
Linaria64 <dorianpro77@proton.me>
2024-08-13 22:18:44 +0200

    save

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+    return (
+      
+        <div className="flex w-full h-screen ">
+          <div className="border-4 border-pink-400 w-2/4"><h1>Partie Gauche</h1></div>
+          <div><h1>Partie droite</h1></div>
+        </div>
+    );
+  }
+  
---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Register() {
+    return (
+      <main>
+        <div>
+          <h1>Bienvenue dans la page Register</h1>
+        </div>
+      </main>
+    );
+  }
+  
